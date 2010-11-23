e468a13c5eb19bcaa302bbdacfbd7ab6fed3b1cd
Jonas Bonér <jonasremove@jonasboner.com>
2010-11-23 14:46:00 +0100

    Initial upload of all akka modules, now separated from the akka core distribution and repository

---
+++ akka-samples/akka-sample-rest-java/src/main/java/sample/rest/java/PersistentSimpleService.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+package sample.rest.java;
+
+public interface PersistentSimpleService {
+  public String count();
+}
---
+++ akka-samples/akka-sample-rest-java/src/main/java/sample/rest/java/SimpleService.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
+
+package sample.rest.java;
+
+public interface SimpleService {
+  public String count();
+}
