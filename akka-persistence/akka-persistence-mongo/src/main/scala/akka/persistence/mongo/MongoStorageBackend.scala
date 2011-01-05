e69145cf6d5803d9715f385684c76c1933a0abcf
Brendan W. McAdams <brendan@10gen.com>
2011-01-04 19:05:50 -0500

    - Support Mongo URI in config - Support Mongo authentication in config & uri - Support Mongo Replication in both Set & Deprecated Pair modes    * Pairs & Replication w/ more than 1 seed host requires use of URI at the moment - Fully support Mongo WriteConcern semantics to allow users to configure exactly how durable Mongo will behave.   * We may want to support an enum here of the names Java driver supports like Cassandra / Hbase does e.g. "SAFE, NORMAL, REPLICA_SAFE, FSYNC_SAFE"   * Default behavior is same as Akka behaves currently - no timeout, w=1, fsync=false - I need to add additional tests for the URI config -- also, if a URI is set it overrides the hostname/port/db settings.

--- akka-persistence/akka-persistence-mongo/src/main/scala/akka/persistence/mongo/MongoStorageBackend.scala
+++ akka-persistence/akka-persistence-mongo/src/main/scala/akka/persistence/mongo/MongoStorageBackend.scala
@@ -10,6 +10,10 @@ import akka.util.Logging
 import akka.config.Config.config
 
 import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.MongoURI 
+import com.mongodb.{ServerAddress, WriteResult}
+
+import scala.util.control.Exception._
 
 /**
  * A module for supporting MongoDB based persistence.
@@ -30,13 +34,132 @@ private[akka] object MongoStorageBackend extends
   val KEY = "__key"
   val REF = "__ref"
   val COLLECTION = "akka_coll"
+  
+  /**
+   * Parse the URI if defined and set the Hostname, dbname and port settings to it
+   * Does not support setting write concern in options yet until Java driver does.
+   */
+  config.getString("akka.persistence.mongodb.uri") match {
+    case None | null | Some("") => {
+      log.debug("No URI configured, deferring config.")        
+    }
+    case Some(uri) => handling(classOf[IllegalArgumentException]) by { e =>
+      throw new Exception("Unable to parse MongoURI '%s': '%s'".format(uri, e.getMessage), e)
+    } apply {
+      val address = MongoURI(uri)
+      if (address.hosts.size == 1) {
+        val host = address.hosts(1)
+        if (address.username != null && !address.username.isEmpty) 
+          config.setString("akka.persistence.mongodb.username", address.username)
+        if (address.password != null && !address.password.isEmpty) 
+          config.setString("akka.persistence.mongodb.username", address.password.mkString)
 
+        if (host.indexOf(':') >= 0) {
+          val x = host.split(':')
+          config.setString("akka.persistence.mongodb.hostname", x(0))
+          config.setString("akka.persistence.mongodb.port", x(1))
+        } else config.setString("akka.persistence.mongodb.hostname", host)
+      } else {
+        // If they give us a list they'll need to enable replica mode or ports get lost
+        config.setString("akka.persistence.mongodb.hostname", address.hosts.mkString(","))
+      }
+    }
+  }
+
+  
+  /** Mongo Config Values */
   val HOSTNAME = config.getString("akka.persistence.mongodb.hostname", "127.0.0.1")
   val DBNAME = config.getString("akka.persistence.mongodb.dbname", "testdb")
   val PORT = config.getInt("akka.persistence.mongodb.port", 27017)
 
-  val db: MongoDB = MongoConnection(HOSTNAME, PORT)(DBNAME)
+  /** Optional Authentication support */
+  val USERNAME = config.getString("akka.persistence.mongodb.username")
+  val PASSWORD = config.getString("akka.persistence.mongodb.password")
+
+  /** 
+   * Support for Replica Pair and Sets modes 
+   * If REPLICA_PAIR is enabled we try to cut on a , into a left and right addresses
+   * If REPLICA_SETS is enabled we parse on , into a List<String> addresses 
+   */
+  val REPLICA_PAIR = config.getBool("akka.persistence.mongodb.replica_mode.pairs", false)
+  val REPLICA_SETS = config.getBool("akka.persistence.mongodb.replica_mode.sets", false)
+
+  /** 
+   * Write concern related config
+   *    For the context of STM, it is best to use a w=1 (same as calling getLastError
+   *    which happens in the request{} blocks.)
+   *    However, many users want to config this and may even want w=2
+   *   @see http://api.mongodb.org/java/2.5-pre-/com/mongodb/WriteConcern.html
+   */
+  /** 
+   * Default 1 -- Wait for 1 server to commit the write before returning.  
+   * If Replica Pairs or Sets is enabled this default will become 2 instead of 1
+   */
+  val WRITE_CONCERN_W = config.getInt("akka.persistence.mongodb.write_concern.w", 
+                                      if (REPLICA_PAIR || REPLICA_SETS) 2 else 1)
+  /** Default 0 -- Wait / block forever for writes to return. */
+  val WRITE_CONCERN_WTIMEOUT = 
+    config.getInt("akka.persistence.mongodb.write_concern.wtimeout", 0)
+
+  /** Defauilt false, if true flushes mongo data to disk on each write before
+   * returning.  Setting this to true should only be done by those who know 
+   * what they are doing
+   */
+  val WRITE_CONCERN_FSYNC = 
+    config.getBool("akka.persistence.mongodb.write_concern_fsync", false)
+
+  val db: MongoDB = getMongoDB 
+
+  // Authenticate if necessary (password can be null technically)
+  USERNAME foreach { login =>
+    require(db.authenticate(login, PASSWORD.getOrElse("")), 
+            "Mongo Authentication failed with provided credentials.")
+  }
+
   val coll: MongoCollection = db(COLLECTION)
+
+  protected def getMongoDB: MongoDB = {
+    log.debug("Setting up MongoConnection")
+    assume(!(REPLICA_PAIR && REPLICA_SETS), 
+          "Replica Pairs and Sets cannot both be enabled: Pick one!")
+
+    def _setAddr(str: String) = if (str.indexOf(':') >= 0) {
+      val h = str.split(':')
+      new ServerAddress(h(0), h(1).toInt)
+    } else new ServerAddress(str)
+
+    val conn = if (REPLICA_PAIR) {
+      log.info("Mongo Replica Pair mode enabled.")
+      val hosts = HOSTNAME.split(',')
+      require(hosts.size == 2, 
+              "Replica Pairs may have no less than or more than 2 members (L, R)")
+
+      val l = _setAddr(hosts(0))
+      log.debug("Left Replica Pair member: %s", l)
+      val r = _setAddr(hosts(1))
+      log.debug("Right Replica Pair member: %s", r)
+      MongoConnection(l, r)
+    } 
+    else if (REPLICA_SETS) {
+      log.info("Mongo Replica Sets mode enabled.")
+      // No requirement on size, replica sets can connect to just one box.
+      // However, if there was no comma sep we need the port
+      if (HOSTNAME.indexOf(',') >= 0) {
+        val hosts = HOSTNAME.split(',')
+        val hostList = hosts.toList map { _setAddr }
+        log.debug("Host list for Replica Set: %s", hostList)
+        MongoConnection(hostList)
+      } else MongoConnection(List(new ServerAddress(HOSTNAME, PORT)))
+    } 
+    else {
+      log.info("Standard Mongo Connection mode enabled.")
+      MongoConnection(HOSTNAME, PORT)
+    }
+    // Setup Write Concern
+    val wc = WriteConcern(WRITE_CONCERN_W, WRITE_CONCERN_WTIMEOUT, WRITE_CONCERN_FSYNC)
+    conn.writeConcern = wc
+    conn(DBNAME)
+  }
 
   def drop() { db.dropDatabase() }
 
@@ -45,24 +168,22 @@ private[akka] object MongoStorageBackend extends
   }
 
   def insertMapStorageEntriesFor(name: String, entries: List[(Array[Byte], Array[Byte])]) {
-    db.request { db =>
-      val q: DBObject = MongoDBObject(KEY -> name)
-      coll.findOne(q) match {
-        case Some(dbo) =>
-          entries.foreach { case (k, v) => dbo += new String(k) -> v }
-          db.request { db => coll.update(q, dbo, true, false) }
-        case None =>
-          val builder = MongoDBObject.newBuilder
-          builder += KEY -> name
-          entries.foreach { case (k, v) => builder += new String(k) -> v }
-          coll += builder.result.asDBObject
-      }
+    val q: DBObject = MongoDBObject(KEY -> name)
+    coll.findOne(q) match {
+      case Some(dbo) =>
+        entries.foreach { case (k, v) => dbo += new String(k) -> v }
+        mongoRequest { _ update(q, dbo, true, false) }
+      case None =>
+        val builder = MongoDBObject.newBuilder
+        builder += KEY -> name
+        entries.foreach { case (k, v) => builder += new String(k) -> v }
+        mongoRequest { _ += builder.result.asDBObject }
     }
   }
 
   def removeMapStorageFor(name: String): Unit = {
     val q: DBObject = MongoDBObject(KEY -> name)
-    db.request { db => coll.remove(q) }
+    mongoRequest { _ remove(q) }
   }
 
 
@@ -74,7 +195,7 @@ private[akka] object MongoStorageBackend extends
   def removeMapStorageFor(name: String, key: Array[Byte]): Unit = queryFor(name) { (q, dbo) =>
     dbo.foreach { d =>
       d -= new String(key)
-      db.request { db => coll.update(q, d, true, false) }
+      mongoRequest { _.update(q, d, true, false) }
     }
   }
 
@@ -129,39 +250,37 @@ private[akka] object MongoStorageBackend extends
     // lookup with name
     val q: DBObject = MongoDBObject(KEY -> name)
 
-    db.request { db =>
-      coll.findOne(q) match {
-        // exists : need to update
-        case Some(dbo) =>
-          dbo -= KEY
-          dbo -= "_id"
-          val listBuilder = MongoDBList.newBuilder
+    coll.findOne(q) match {
+      // exists : need to update
+      case Some(dbo) =>
+        dbo -= KEY
+        dbo -= "_id"
+        val listBuilder = MongoDBList.newBuilder
 
-          // expensive!
-          listBuilder ++= (elements ++ dbo.toSeq.sortWith((e1, e2) => (e1._1.toInt < e2._1.toInt)).map(_._2))
+        // expensive!
+        listBuilder ++= (elements ++ dbo.toSeq.sortWith((e1, e2) => (e1._1.toInt < e2._1.toInt)).map(_._2))
 
-          val builder = MongoDBObject.newBuilder
-          builder += KEY -> name
-          builder ++= listBuilder.result
-          coll.update(q, builder.result.asDBObject, true, false)
+        val builder = MongoDBObject.newBuilder
+        builder += KEY -> name
+        builder ++= listBuilder.result
+        mongoRequest { _ update(q, builder.result.asDBObject, true, false) }
 
-        // new : just add
-        case None =>
-          val listBuilder = MongoDBList.newBuilder
-          listBuilder ++= elements
+      // new : just add
+      case None =>
+        val listBuilder = MongoDBList.newBuilder
+        listBuilder ++= elements
 
-          val builder = MongoDBObject.newBuilder
-          builder += KEY -> name
-          builder ++= listBuilder.result
-          coll += builder.result.asDBObject
-      }
+        val builder = MongoDBObject.newBuilder
+        builder += KEY -> name
+        builder ++= listBuilder.result
+        mongoRequest { _ += builder.result.asDBObject }
     }
   }
 
   def updateVectorStorageEntryFor(name: String, index: Int, elem: Array[Byte]) = queryFor(name) { (q, dbo) =>
     dbo.foreach { d =>
       d += ((index.toString, elem))
-      db.request { db => coll.update(q, d, true, false) }
+      mongoRequest { _.update(q, d, true, false) }
     }
   }
 
@@ -204,20 +323,18 @@ private[akka] object MongoStorageBackend extends
     // lookup with name
     val q: DBObject = MongoDBObject(KEY -> name)
 
-    db.request { db =>
-      coll.findOne(q) match {
-        // exists : need to update
-        case Some(dbo) =>
-          dbo += ((REF, element))
-          coll.update(q, dbo, true, false)
+    coll.findOne(q) match {
+      // exists : need to update
+      case Some(dbo) =>
+        dbo += ((REF, element))
+        mongoRequest { _ update(q, dbo, true, false) }
 
-        // not found : make one
-        case None =>
-          val builder = MongoDBObject.newBuilder
-          builder += KEY -> name
-          builder += REF -> element
-          coll += builder.result.asDBObject
-      }
+      // not found : make one
+      case None =>
+        val builder = MongoDBObject.newBuilder
+        builder += KEY -> name
+        builder += REF -> element
+        mongoRequest { _ += builder.result.asDBObject }
     }
   }
 
@@ -226,4 +343,16 @@ private[akka] object MongoStorageBackend extends
       d.getAs[Array[Byte]](REF)
     }.getOrElse(None)
   }
+
+
+  /** 
+   * Utility method similar to Casbah's request() operator but 
+   * uses the WriteConcern / WriteResult semantic instead. 
+   * Wraps a write operation block (Block must return WriteResult) e.g.
+   * last call is to save/insert/update/remove and throws any error.
+   *
+   * Casbah will have a version of this in a future release
+   */
+   protected def mongoRequest(op: MongoCollection => WriteResult) = 
+    op(coll).getLastError.throwOnError
 }
