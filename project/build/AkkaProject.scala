47efbc8dbd8c30c1db3513b2e3e983f48c0c5320
Martin Krasser <krasserm@googlemail.com>
2011-03-17 12:38:01 +0100

    Preliminary upgrade to latest Camel development snapshot.

--- project/build/AkkaProject.scala
+++ project/build/AkkaProject.scala
@@ -124,16 +124,16 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
 
   lazy val AKKA_VERSION          = "1.1-SNAPSHOT"
   lazy val ATMO_VERSION          = "0.6.2"
-  lazy val CAMEL_VERSION         = "2.5.0"
+  lazy val CAMEL_VERSION         = "2.8-SNAPSHOT"
   lazy val DISPATCH_VERSION      = "0.7.4"
   lazy val HAWT_DISPATCH_VERSION = "1.1"
   lazy val JACKSON_VERSION       = "1.7.1"
   lazy val JERSEY_VERSION        = "1.3"
   lazy val MULTIVERSE_VERSION    = "0.6.2"
   lazy val SCALATEST_VERSION     = "1.3"
-  lazy val SLF4J_VERSION         = "1.6.0"
-  lazy val SPRING_VERSION        = "3.0.4.RELEASE"
-  lazy val JETTY_VERSION         = "7.1.6.v20100715"
+  lazy val SLF4J_VERSION         = "1.5.11"
+  lazy val SPRING_VERSION        = "3.0.5.RELEASE"
+  lazy val JETTY_VERSION         = "7.2.2.v20101205"
   lazy val CODEC_VERSION         = "1.4"
 
   // -------------------------------------------------------------------------------------------------------------------
@@ -207,6 +207,8 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
 
     lazy val google_coll    = "com.google.collections" % "google-collections"  % "1.0"             % "compile" //ApacheV2
 
+    lazy val slf4j          = "org.slf4j"              % "slf4j-simple"        % SLF4J_VERSION     % "compile" // MIT
+
     // Test
 
     lazy val camel_spring   = "org.apache.camel"       % "camel-spring"        % CAMEL_VERSION     % "test" //ApacheV2
@@ -262,17 +264,12 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
     " config/" +
     " scala-library.jar" +
     " dist/akka-camel-%s.jar".format(version) +
+    " dist/akka-camel-typed-%s.jar".format(version) +
     " dist/akka-amqp-%s.jar".format(version) +
     " dist/akka-kernel-%s.jar".format(version) +
     " dist/akka-spring-%s.jar".format(version) +
     " dist/akka-scalaz-%s.jar".format(version)
     )
-
-  //Exclude slf4j1.5.11 from the classpath, it's conflicting...
-  override def fullClasspath(config: Configuration): PathFinder = {
-    super.fullClasspath(config) ---
-    (super.fullClasspath(config) ** "slf4j*1.5.11.jar")
-  }
 
   override def mainResources = super.mainResources +++
           (info.projectPath / "config").descendentsExcept("*", "logback-test.xml")
@@ -373,10 +370,12 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
   class AkkaCamelProject(info: ProjectInfo) extends AkkaModulesDefaultProject(info, distPath) {
     val akka_actor = Dependencies.akka_actor
     val camel_core = Dependencies.camel_core
+    val slf4j      = Dependencies.slf4j
 
     // testing
     val junit     = Dependencies.junit
     val scalatest = Dependencies.scalatest
+
     override def testOptions = createTestFilter( _.endsWith("Test"))
   }
 
@@ -391,6 +390,7 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
     // testing
     val junit     = Dependencies.junit
     val scalatest = Dependencies.scalatest
+
     override def testOptions = createTestFilter( _.endsWith("Test"))
   }
 
@@ -575,7 +575,7 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
         </dependency>
         <dependency org="org.apache.camel" name="camel-jms" rev={CAMEL_VERSION}>
         </dependency>
-        <dependency org="org.apache.activemq" name="activemq-core" rev="5.3.2">
+        <dependency org="org.apache.activemq" name="activemq-core" rev="5.4.2">
         </dependency>
       </dependencies>
 
