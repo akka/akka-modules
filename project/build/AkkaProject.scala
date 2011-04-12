17a033f794ff56d45290e76e72cd37ca12df2f71
ticktock <ticktock@ticktock.com>
2011-04-11 17:43:45 -0700

    remove -Xstrict-warnings from compile options

--- project/build/AkkaProject.scala
+++ project/build/AkkaProject.scala
@@ -35,7 +35,6 @@ class AkkaModulesParentProject(info: ProjectInfo) extends DefaultProject(info) {
   override def compileOptions = super.compileOptions ++
     Seq("-deprecation",
         "-Xmigration",
-        "-Xstrict-warnings",
         "-optimise", //Uncomment this for release compile
         "-encoding", "utf8")
         .map(CompileOption(_))
