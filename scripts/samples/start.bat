a133e3f7eed492527bec24bf4c96d8dc67be695a
Peter Vlugter <peter@maka>
2011-05-12 08:33:17 +1200

    Update windows scripts

--- scripts/samples/start.bat
+++ scripts/samples/start.bat
@@ -1,7 +1,8 @@
+@echo off
 set SAMPLE=%~dp0..
-set AKKA_HOME=%SAMPLE%\\..\\..\\..\\..
-set JAVA_OPTS="-Xms1536M -Xmx1536M -Xss1M -XX:MaxPermSize=256M -XX:+UseParallelGC"
-set AKKA_CLASSPATH="%AKKA_HOME%\\lib\\scala-library.jar;%AKKA_HOME%\\lib\\akka\\*"
-set SAMPLE_CLASSPATH="%AKKA_CLASSPATH%;%SAMPLE%\\lib\\*;%SAMPLE%\\config"
+set AKKA_HOME=%SAMPLE%\..\..\..\..
+set JAVA_OPTS=-Xms1536M -Xmx1536M -Xss1M -XX:MaxPermSize=256M -XX:+UseParallelGC
+set AKKA_CLASSPATH=%AKKA_HOME%\lib\scala-library.jar;%AKKA_HOME%\lib\akka\*
+set SAMPLE_CLASSPATH=%AKKA_CLASSPATH%;%SAMPLE%\lib\*;%SAMPLE%\config
 
-java %JAVA_OPTS% -cp "%SAMPLE_CLASSPATH%" -Dakka.home=%SAMPLE% akka.kernel.Main
+java %JAVA_OPTS% -cp "%SAMPLE_CLASSPATH%" -Dakka.home="%SAMPLE%" akka.kernel.Main
